6ba8892166bedb2462862cbadf779c2ec75ae83a
Sebastian Wypiór <wypior.sebastian@gmail.com>
2022-03-02 07:42:13 +0100

    Add claims implementation

--- src/Core/SquashLeagueService.Application/Identities/Commands/Signup/SignupCommand.cs
+++ src/Core/SquashLeagueService.Application/Identities/Commands/Signup/SignupCommand.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using System.Security.Claims;
+using MediatR;
 using Microsoft.AspNetCore.Identity;
 using SquashLeagueService.Application.Common.Exceptions;
 using SquashLeagueService.Domain.Entities;
@@ -12,6 +13,8 @@ public class SignupCommand : IRequest<SignupResponse>
     public string FirstName { get; init; }
     public string LastName { get; init; }
     public string Email { get; init; }
+    public string Role { get; init; }
+    public string PhoneNumber { get; init; }
     public string Password { get; init; }
 }
 
@@ -36,7 +39,37 @@ public class SignupQueryHandler : IRequestHandler<SignupCommand, SignupResponse>
             if (!roleCreationResult.Succeeded)
                 throw new RoleRegistrationException(role.Name);
         }
+
+        await VerifyUserExistence(request);
+
+        var createdUser = await CreateApplicationUser(request);
         
+        await _userManager.AddToRoleAsync(createdUser, "Player");
+        await AddUserClaims(createdUser, request);
+
+        return new SignupResponse { Id = createdUser.Id};
+    }
+
+    private async Task<ApplicationUser> CreateApplicationUser(SignupCommand request)
+    {
+        var newUser = new ApplicationUser()
+        {
+            UserName = request.Username,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            PhoneNumber = request.PhoneNumber,
+            Email = request.Email
+        };
+
+        var userCreationResult = await _userManager.CreateAsync(newUser, request.Password);
+        if (!userCreationResult.Succeeded)
+            throw new UserRegistrationException("Registration error", userCreationResult.Errors);
+        
+        return await _userManager.FindByNameAsync(request.Username);
+    }
+
+    private async Task VerifyUserExistence(SignupCommand request)
+    {
         var existingUser = await _userManager.FindByNameAsync(request.Username);
         if (existingUser is { })
             throw new UserAlreadyExistsException(request.Username);
@@ -44,23 +77,15 @@ public class SignupQueryHandler : IRequestHandler<SignupCommand, SignupResponse>
         var existingEmail = await _userManager.FindByEmailAsync(request.Email);
         if (existingEmail is { })
             throw new UserAlreadyExistsException(request.Email);
+    }
 
-        var newUser = new ApplicationUser()
-        {
-            UserName = request.Username,
-            FirstName = request.FirstName,
-            LastName = request.LastName,
-            Email = request.Email
-        };
+    private Task AddUserClaims(ApplicationUser applicationUser, SignupCommand request)
+    {
+        var tasks = new List<Task>();
+        tasks.Add(_userManager.AddClaimAsync(applicationUser, new Claim("Phone", request.PhoneNumber)));
+        tasks.Add(_userManager.AddClaimAsync(applicationUser, new Claim("Email", request.Email)));
 
-        var userCreationResult = await _userManager.CreateAsync(newUser, request.Password);
-        if (!userCreationResult.Succeeded)
-            throw new UserRegistrationException("Registration error", userCreationResult.Errors);
-
-        var createdUser = await _userManager.FindByNameAsync(request.Username);
-        await _userManager.AddToRoleAsync(createdUser, "Player");
-
-        return new SignupResponse { Id = createdUser.Id};
+        return Task.WhenAll(tasks);
     }
 }
 
