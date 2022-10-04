01faa8d6acd453399fc35179f86ac43cc5488c08
Sebastian Wypiór <wypior.sebastian@gmail.com>
2022-10-04 07:26:38 +0200

    Add domain abstractions

---
+++ src/Core/SquashLeagueService.Domain/Abstractions/Entity.cs
@@ -0,0 +1,42 @@
+﻿namespace SquashLeagueService.Domain.Abstractions;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; set; }
+    
+    protected Entity(Guid id) => Id = id;
+    protected Entity(){}
+
+    public static bool operator ==(Entity? left, Entity? right)
+        => left is not null && right is not null && left.Equals(right);
+
+    public static bool operator !=(Entity? left, Entity right)
+        => !(left == right);
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+
+        if (other.GetType() != GetType())
+            return false;
+
+        return other.Id == Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        
+        if (obj.GetType() != GetType())
+            return false;
+
+        if (obj is not Entity entity)
+            return false;
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode() => Id.GetHashCode() * 41;
+}
